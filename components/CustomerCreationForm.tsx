648d1b3de8a5268750ddcb17b1bc95499f74931c
Jakob <jakob.dugan@outlook.com>
2025-05-24 10:46:10 -0500

    added MUI

--- components/CustomerCreationForm.tsx
+++ components/CustomerCreationForm.tsx
@@ -1,7 +1,10 @@
+import Button from '@mui/material/Button';
 
 export default function CustomerCreationForm() {
     return(
         <>
             <h1><b>THIS IS STRAIGHT OUTA COMPONENTS</b></h1>
-        </>)
+            <Button variant="contained">Create Customer
+        </Button>
+            </>)
 }
