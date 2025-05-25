2fc7bbcac3898629b52598031549ed6b498b7137
Jakob <jakob.dugan@outlook.com>
2025-05-24 23:38:07 -0500

    completed form with Implicit Event

--- iofunctions/createCustomerIO.tsx
+++ iofunctions/createCustomerIO.tsx
@@ -1,6 +1,31 @@
+import type { Schema } from '../amplify/data/resource'
+import { generateClient } from 'aws-amplify/data'
 
-function createCustomerIO(formData: { customerName: string; customerPhone: string; customerEmail: string; customerAddress1: string; customerAddress2: string; customerCity: string; customerState: string; customerZip: string; }){
+const client = generateClient<Schema>()
+
+
+const createCustomerIO= async (
+    formData: {
+        customerName: string;
+        customerPhone: string;
+        customerEmail: string;
+        customerAddress1: string;
+        customerAddress2: string;
+        customerCity: string;
+        customerState: string;
+        customerZip: string; }) =>{
+    await client.models.Customer.create({
+        customerName: formData.customerName,
+        customerPhone: formData.customerPhone,
+        customerEmail: formData.customerEmail,
+        customerAddress1: formData.customerAddress1,
+        customerAddress2: formData.customerAddress2,
+        customerCity: formData.customerCity,
+        customerState: formData.customerState,
+        customerZip: formData.customerZip,
+    })
     console.log("!!!create customer!!!");
     console.log(formData.customerName);
+
 }
 export default createCustomerIO;
