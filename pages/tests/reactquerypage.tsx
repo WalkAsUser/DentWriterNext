66df353c46c23ce394a8e54be6c25aeb027b2a15
Jakob <jakob.dugan@outlook.com>
2025-08-02 07:06:10 -0500

    react query display no return

--- pages/tests/reactquerypage.tsx
+++ pages/tests/reactquerypage.tsx
@@ -25,7 +25,7 @@ export default function ReactQueryPage() {
             //if (isError) return <div>Error: {error.message}</div>;
             return (
                 <ul>
-                    {allCustomers.items.map((customer) => (
+                    {allCustomers.items?.map((customer) => (
                         <li key={customer.id}>{customer.customerName}</li>
                     ))}
                 </ul>
