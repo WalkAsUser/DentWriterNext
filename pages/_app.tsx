c170b4cf7015c8f751a24f7d092525f483617e60
Jakob <jakob.dugan@outlook.com>
2025-08-02 04:46:24 -0500

    Added ReactQuery "tanstack"

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,13 +6,19 @@ import type { AppProps } from "next/app";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
+//This is Adding ReactQuery or rather TanStack//
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+const queryClient = new QueryClient();
 Amplify.configure(outputs);
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-      <Authenticator>
-      <Component {...pageProps} />;
-      </Authenticator>
+      <QueryClientProvider client={queryClient}>
+        <Authenticator>
+            <Component {...pageProps} />;
+        </Authenticator>
+      </QueryClientProvider>
   )
 }
